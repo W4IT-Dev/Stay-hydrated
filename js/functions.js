3125632334d9a03a3e180aa91448d8545080f95a
W4IT <w4it.dev.business@gmail.com>
2024-02-06 20:07:26 +0100

    i think i got it

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,5 @@
+let DONTSWITCH = false;
+
 function toggleSelectedItem(parentElement, changeTo) {
     let items = parentElement.querySelectorAll('.item')
     if (changeTo) {
@@ -20,18 +22,33 @@ function toggleSelectedItem(parentElement, changeTo) {
         });
     }
     if (parentElement === weightUnit.parent) weightUnit.value = parentElement.querySelector('.item.selected').innerText, changeUnit(weightUnit, weightUnit.value, true);
-    else fluidUnit.value = parentElement.querySelector('.item.selected').innerText, changeUnit(fluidUnit, fluidUnit.value), calculateWaterIntake();
+    else DONTSWITCH = true, fluidUnit.value = parentElement.querySelector('.item.selected').innerText, changeUnit(fluidUnit, fluidUnit.value), calculateWaterIntake();
 }
 
 function calculateWaterIntake() {
+    // Calculate oz/lbs
+    // Convert to ml/kg if needed
     let goal;
-    if (weightUnit.value == "kg") {
-        goal = weight.value * 32.5
-        if (fluidUnit.value == "oz") goal *= 0.033814
-    } else if(weightUnit.value == "lbs") {
-        goal = weight.value * 0.65
-        if(fluidUnit.value == "ml") goal *= 29,5735
+
+    // CALC NEED
+    if (weightUnit.value == "lbs") {
+        goal = weight.value * 0.65;//if unit is already LBS
+    } else if (weightUnit.value == "kg") {
+        goal = (weight.value * 2.20462).toFixed(1) * 0.65//if unit is KG
+        // console.log(goal)
     }
+
+    // CONVERT
+    if (fluidUnit.value == "ml") parseInt(goal *= 29.5753);
+
+    // if (weightUnit.value == "kg") {
+    //     goal = weight.value * 32.5
+    //     if (fluidUnit.value == "oz") goal *= 0.033814
+    // } else if(weightUnit.value == "lbs") {
+    //     goal = weight.value * 0.65
+    //     if(fluidUnit.value == "ml") goal *= 29.5735
+    // }
+    // TODO: Calculate oz/lbs and then convert it to ml/kg; because this is too inconsitenst.
     // return goal
     waterIntakeGoalAuto.calculatedWaterIntakeDisplay.innerText = goal.toFixed(2) + fluidUnit.value;
     return goal.toFixed(1) + fluidUnit.value
@@ -44,7 +61,7 @@ function updateWaterIntake(amount) {
 
 function changeUnit(unitOriginal, toUnit, inSetup) {
     unitOriginal.value = toUnit;
-    if (inSetup && unitOriginal === weightUnit) {
+    if (inSetup && unitOriginal === weightUnit && !DONTSWITCH) {
 
         if (weightUnit.value == 'kg') {
             fluidUnit.value = 'ml';
@@ -53,7 +70,7 @@ function changeUnit(unitOriginal, toUnit, inSetup) {
         } else if (weightUnit.value == 'lbs') {
             fluidUnit.value = 'oz';
             weight.value = (weight.value * 2.20462).toFixed(1);
-            toggleSelectedItem(fluidUnit.parent, 'oz');
+           toggleSelectedItem(fluidUnit.parent, 'oz');
         }
         calculateWaterIntake();
     }
